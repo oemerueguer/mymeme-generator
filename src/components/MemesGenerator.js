06476bc981c361e44433b6177b4541016a9c38ed
Lodos <o.ueguer@gmail.com>
2021-05-17 23:49:52 +0200

    Creates new pic

--- src/components/MemesGenerator.js
+++ src/components/MemesGenerator.js
@@ -1,30 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import "../components/MemeStyle.css";
 
+export default function Memes() {
+  const [picture, setPicture] = useState();
 
-export default function Memes(){
-    const [picture, setPicture] = useState('');
+  useEffect(() => {
+    fetchPicture();
+  }, []);
 
-    useEffect(() => {
-        fetchPicture();
-    }, []);
+  const randomNumber = Math.floor(Math.random() * 100);
 
-    const fetchPicture = async () => {
-        await Axios.get("https://api.imgflip.com/get_memes")
-        .then((response) => console.log(response.data.data))
-        .catch((error) => console.log(error))
-    };
+  const fetchPicture = async () => {
+    await Axios.get("https://api.imgflip.com/get_memes")
+      .then((response) =>
+        setPicture(response.data.data.memes[randomNumber].url)
+      )
+      .catch((error) => console.log(error));
+  };
 
-    return(
-        <div className="App">
-            <h1>Meme Generator</h1>
-            <div className="flex">
-                <input></input>
-                <button></button>
-            </div>
-            <img src={picture.memes} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Meme Generator</h1>
+      <input></input>
+      <button onClick={fetchPicture}>Generate</button>
+
+      <div className="generatedPic">
+        {picture && <img className="memePic" src={picture} />}
+      </div>
+    </div>
+  );
 }
-    
-
